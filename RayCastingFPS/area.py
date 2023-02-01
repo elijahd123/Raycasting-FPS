09ccef2a85fe7350a275932ce1c7db53153fb2f1
elijahd123 <69352560+elijahd123@users.noreply.github.com>
2023-02-01 22:56:11 +0000

    Delete RayCastingFPS directory

--- RayCastingFPS/area.py
+++
@@ -1,7 +0,0 @@
-class Area:
-    def __init__(self, area):
-        self.block_char = "#"
-        self.area = [list(row) for row in area]
-        """self.width = len(self.area[-1])
-        self.height = len(self.area)
-        self.block_width = """
